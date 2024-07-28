eb7cf9bc46fabd379ee34051fd1e22fc6374e4eb
Hakan KARAYILMAZ <hkankyilmazz@gmail.com>
2024-07-28 21:47:18 +0300

    created userAuthStore

--- src/stores/user.js
+++ src/stores/user.js
@@ -3,6 +3,27 @@ import { defineStore } from 'pinia'
 import useAuthUser from '@/services/AuthService'
 
 
+export const useAuthStore = defineStore('user', {
+    state: () => ({
+        user: null,
+    }),
+    getters: {
+        getUser() {
+            return this.user;
+        },
+        isLoggedIn() {
+            return this.user.id;
+        }
+    },
+    actions: {
+        setUser(user) {
+            this.user = user;
+        },
+
+    },
+
+})
+
 
 
 
