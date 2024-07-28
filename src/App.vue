11a677dd75628dd7a3816c168ab19a4c6e03eb5c
Hakan KARAYILMAZ <hkankyilmazz@gmail.com>
2024-07-28 16:49:30 +0300

    created project, layout and some supabase funcs

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import { onMounted, ref } from "vue";
+import "./assets/main.css";
+import logo from "@/assets/logo.png";
+
+const appLogo = logo;
+
+const header = ref(null);
+const footer = ref(null);
+const mainContent = ref(null);
+
+onMounted(() => {
+  const headerHeight = header.value.offsetHeight;
+  const footerHeight = footer.value.offsetHeight;
+  const totalHeight = headerHeight + footerHeight;
+  const h = `calc(100vh - ${totalHeight}px)`;
+  mainContent.value.style.height = h;
+});
+</script>
+
+<template lang="pug">
+
+main(class="flex flex-col min-h-[100vh] m-auto justify-between max-w-[1200px] bg-[#FAFAFA] shadow-lg")
+
+  header(class="bg-gray-800  text-white  flex justify-between items-center px-3 md:px-10" ref="header")
+      div
+        img(:src="appLogo" alt="Vue logo" width="70" height="50" class="cursor-pointer" @click="handleClick")
+      nav(class="flex space-x-3 md:space-x-5") 
+          RouterLink(to="/") Home
+          RouterLink(v-if="!isSessionExist" to="/login") Login
+          span(v-if="isSessionExist") Welcome
+          span(v-if="isSessionExist" @click="logOutUser" class="cursor-pointer") Log Out
+  div(class="grid grid-cols-[auto_300px]  gap-3" ref='mainContent')
+      RouterView(class="border")
+      aside(class="flex flex-col")
+        div(class="h-[50%] border") Top Stars Author
+        div(class="h-[50%] border") Top Star Blog
+  footer(class="bg-gray-800  text-white flex justify-center items-center p-5" ref="footer")
+      p(class="text-sm") © 2024 bloggApp  - Hakan KARAYILMAZ
+  
+
+</template>
---
+++ src/SQL/seed.sql
@@ -0,0 +1,26 @@
+
+/**
+    * Seed data for the database
+*/
+
+CREATE TABLE blog (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT[] NOT NULL,
+    author_id INT NOT NULL,
+    likes INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (author_id) REFERENCES users (id)
+);
+
+CREATE TABLE likes (
+    id SERIAL PRIMARY KEY,
+    blog_id INT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (blog_id) REFERENCES blog (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+
+
+
